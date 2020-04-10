61d90636795b3b17439e3b4225b620a7122f9ada
Cesar Tomatis <cesartomatis@outlook.com>
2020-04-10 22:03:20 +0200

    homepage added

--- src/components/Navigation/SideMenu/DownloadResumeItem/DownloadResumeItem.js
+++ src/components/Navigation/SideMenu/DownloadResumeItem/DownloadResumeItem.js
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 
 import classes from './DownloadResumeItem.module.scss';
 import { I18nContext } from '../../../../i18n';
+import CV from '../../../../assets/cv/CV - Cesar Tomatis.pdf';
 
 const DownloadResumeItem = (props) => {
 	const { translate } = useContext(I18nContext);
 
 	return (
-		<div className={classes.Container}>
+		<div className={classes.Container} onClick={() => window.open(CV)}>
 			<span className={['material-icons', classes.Icon].join(' ')}>
 				cloud_download
 			</span>
